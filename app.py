03af9e020188a49f3ea5cfac4f1830d339c9a90b
Александр Уланов <a.ulanov@list.ru>
2022-12-03 22:36:37 +0600

    Created a simple Flask appliacation

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
+from datetime import timedelta
+
 from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resouces.user import UserRegister
+from resouces.item import Item, ItemList
+from resouces.store import Store, StoreList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+api = Api(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
+
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+jwt = JWT(app, authenticate, identity)
+
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    app.run()
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
